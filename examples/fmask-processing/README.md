55bf801bde5bf66c6962c5de80da0a5fb15e066f
Felipe Carlos <efelipecarlos@gmail.com>
2022-11-12 17:41:34 -0300

    example: adding example using a binary tool with workbench

---
+++ examples/fmask-processing/README.md
@@ -0,0 +1,15 @@
+## Fmask reproducible processing
+
+## Running
+
+**1. Build the docker image**
+
+```sh
+docker-compose build
+```
+
+**2. Run the container**
+
+```sh
+docker-compose up
+```
---
+++ examples/fmask-processing/run.sh
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Storm Project.
+#
+# storm-workbench is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+
+fmask_sentinel2Stacked.py \
+    --safedir data/raw_data/S2A_MSIL1C_20221110T140711_N0400_R110_T21MWU_20221110T155128.SAFE/ \
+    -o data/derived_data/S2A_MSIL1C_20221110T140711_N0400_R110_T21MWU_20221110T155128_FMASK.tif
