20df35165ccef1ece5b798d6476b67d3bc5cb72c
Felipe Carlos <efelipecarlos@gmail.com>
2022-11-14 00:29:02 -0300

    example: use virtualenv to improve reproducibility

--- examples/temporal-analysis/bootstrap/base.sh
+++ examples/temporal-analysis/bootstrap/base.sh
@@ -20,4 +20,5 @@ apt-get install -y \
             libsqlite3-dev wget curl \
             llvm libncursesw5-dev xz-utils \
             tk-dev libxml2-dev libxmlsec1-dev \
-            libffi-dev liblzma-dev libcairo2-dev
+            libffi-dev liblzma-dev libcairo2-dev \
+            python3.8-venv
